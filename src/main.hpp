8b440b95629113472cfb0923040d63ff16bf95bd
Julian Stobbe <atomtomate@gmx.de>
2018-05-23 11:15:00 +0200

    tan Math and R Inside temporarily disabled, next: cmake pull trng

--- src/main.hpp
+++ src/main.hpp
@@ -20,8 +20,8 @@
 //#include <RInside.h>
 //#include <Rcpp.h>
 //#include <RcppEigen.h>
+//#include <stan/math.hpp>
 #include <Eigen/Dense>
-#include <stan/math.hpp>
 #include <boost/mpi/environment.hpp>
 #include <boost/mpi/communicator.hpp>
 
