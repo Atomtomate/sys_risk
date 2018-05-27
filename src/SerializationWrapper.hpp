2388f2136ea7f699bc9775f800b1f164af48e41c
Julian Stobbe <atomtomate@gmx.de>
2018-05-27 18:47:00 +0200

    afasd

--- src/SerializationWrapper.hpp
+++ src/SerializationWrapper.hpp
@@ -13,6 +13,19 @@
 class SerializationWrapper {
 
 };
+/*
+char const* greet()
+{
+    return "hello, world";
+}
 
+#include <boost/python.hpp>
+
+BOOST_PYTHON_MODULE(hello)
+{
+    using namespace boost::python;
+    def("greet", greet);
+}
+ */
 
 #endif //VALUATION_SERIALIZATIONWRAPPER_HPP
