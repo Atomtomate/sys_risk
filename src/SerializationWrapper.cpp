670220eaf9f8ca6b6a7aee7a4d1f6e419465ed2b
Julian Stobbe <atomtomate@gmx.de>
2018-05-27 18:35:52 +0200

    error in cmake

---
+++ src/SerializationWrapper.cpp
@@ -0,0 +1,9 @@
+/* Copyright (C) 5/27/18 Julian Stobbe - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ *
+ * You should have received a copy of the MIT license with
+ * this file.
+ */
+
+#include "SerializationWrapper.hpp"
---
+++ src/SerializationWrapper.hpp
@@ -0,0 +1,18 @@
+/* Copyright (C) 5/27/18 Julian Stobbe - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ *
+ * You should have received a copy of the MIT license with
+ * this file.
+ */
+
+#ifndef VALUATION_SERIALIZATIONWRAPPER_HPP
+#define VALUATION_SERIALIZATIONWRAPPER_HPP
+
+
+class SerializationWrapper {
+
+};
+
+
+#endif //VALUATION_SERIALIZATIONWRAPPER_HPP
